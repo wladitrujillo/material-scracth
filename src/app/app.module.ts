dd9024052dfe4447d80753c92ec26b6c9f94b141
Nelson Trujillo <ntrujillo@cobiscorp.com>
2020-02-13 14:15:20 -0500

    NTR-feat:Angular Material Navigation And Containers

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,8 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
+import { TopMenuComponent } from './top-menu/top-menu.component';
+import { CoursesCardListComponent } from './courses-card-list/courses-card-list.component';
 
 const MaterialModules = [
   MatMenuModule,
@@ -30,7 +32,9 @@ const MaterialModules = [
   declarations: [
     AppComponent,
     HomeComponent,
-    AboutComponent
+    AboutComponent,
+    TopMenuComponent,
+    CoursesCardListComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/courses-card-list/courses-card-list.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { Course } from '../model/course';
+
+@Component({
+  selector: 'app-courses-card-list',
+  templateUrl: './courses-card-list.component.html',
+  styleUrls: ['./courses-card-list.component.css']
+})
+export class CoursesCardListComponent implements OnInit {
+
+
+  @Input()
+  courses:Course[];
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
